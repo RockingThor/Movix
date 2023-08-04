153d20b267bddab42aab17a393470a694d6b311e
Rohit Nandi <rohitnandibmp@gmail.com>
2023-08-04 22:09:19 +0530

    hero banner is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,46 @@
 import { useEffect } from 'react'
 import './App.css'
 import {fetchDataFromApi} from './utils/api'
+import { useDispatch, useSelector } from 'react-redux'
+import { getApiConfiguration } from './store/homeSlice';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Details from './pages/details/Details';
+import SearchResult from './pages/searchResult/SearchResult';
+import Explore from './pages/explore/Explore';
+import PageNotFound from './pages/404/PageNotFound';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+
+
 
 function App() {
-  const ApiTesting =async()=>{
-    const response= await fetchDataFromApi('/movie/popular');
-    console.log(response);
+  const dispatch = useDispatch();
+  const {url} =useSelector((state)=>state.home);
+  const fetchApiConfig =async()=>{
+    const response= await fetchDataFromApi('/configuration');
+    const url ={
+      backdrop: response.images.secure_base_url + "original",
+      poster: response.images.secure_base_url + "original",
+      profile: response.images.secure_base_url + "original"
+    };
+    dispatch(getApiConfiguration(url));
   }
   useEffect(()=>{
-    ApiTesting();
+    fetchApiConfig();
   },[])
   return (
-    <div className="App">
-      App
-    </div>
+    <BrowserRouter>
+      <Header/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/:mediaType/:id' element={<Details/>}/>
+          <Route path='/search/:query' element={<SearchResult/>}/>
+          <Route path='/explore/:mediaType' element={<Explore/>}/>
+          <Route path='*' element={<PageNotFound/>}/>
+        </Routes>
+      <Footer/>
+    </BrowserRouter>
   )
 }
 
