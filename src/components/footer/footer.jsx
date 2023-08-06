9b84a78531aac4ed7643d612d28a5fead45cb9f5
Rohit Nandi <rohitnandibmp@gmail.com>
2023-08-07 04:25:20 +0530

    final commit

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,6 +4,7 @@ import {
     FaInstagram,
     FaTwitter,
     FaLinkedin,
+    FaGithub,
 } from "react-icons/fa";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
@@ -22,24 +23,49 @@ const Footer = () => {
                     <li className="menuItem">FAQ</li>
                 </ul>
                 <div className="infoText">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
+                    Don't know what should I write here. I could place simple
+                    Lorem Ipsum text but I decided to waste your and my time
+                    here. I used React to develop this site. Used Redux to
+                    manage the states. Don't know why I'm still writing this
+                    thing. Why are you still reading this thing. Please give a
+                    look to all the responsive and good looking features I have
+                    added in this site. Yes, this site takes time to load data.
+                    I know this and beleive me it's not my fault. Actually I'm
+                    fetching data from TMDB APIs and they take time to send the
+                    response. Please hire me, I'm broken.
                 </div>
                 <div className="socialIcons">
-                    <span className="icon">
-                        <FaFacebookF />
+                    <span
+                        className="icon"
+                        onClick={() => {
+                            window.open(
+                                "https://github.com/RockingThor",
+                                "_blank"
+                            );
+                        }}
+                    >
+                        <FaGithub />
                     </span>
-                    <span className="icon">
-                        <FaInstagram />
-                    </span>
-                    <span className="icon">
+                    <span
+                        className="icon"
+                        onClick={() => {
+                            window.open(
+                                "https://twitter.com/oirohit",
+                                "_blank"
+                            );
+                        }}
+                    >
                         <FaTwitter />
                     </span>
-                    <span className="icon">
+                    <span
+                        className="icon"
+                        onClick={() => {
+                            window.open(
+                                "https://www.linkedin.com/in/rohitnandi/",
+                                "_blank"
+                            );
+                        }}
+                    >
                         <FaLinkedin />
                     </span>
                 </div>
