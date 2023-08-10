c9f7689e3cf038adc26c8a2f4f9e8e35cfee3802
Rohit Nandi <121621183+RockingThor@users.noreply.github.com>
2023-08-11 03:37:46 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL="https://api.themoviedb.org/3";
-const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+const TMDB_TOKEN = process.env.TMDB_API_KEY;
 
 const headers ={
     Authorization: "bearer "+ TMDB_TOKEN
